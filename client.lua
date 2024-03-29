8f7959c5393580e0f9fb7c01d21830aafc7578b0
Delfin8559 <164158308+Delfin8559@users.noreply.github.com>
2024-03-29 12:01:59 +0100

    t

--- client.lua
+++ client.lua
@@ -1,1 +1,38 @@
+local isNearCoords = false
 
+Citizen.CreateThread(function()
+    while true do
+
+        playerPos = GetEntityCoords(PlayerPedID())
+
+        isNearCoords = false
+        for k, v in pairs(Config.Locations) do
+            local distance = Vdist(playerPos, v[1], v[2], v[3])
+
+            if distance <= 1.5 then
+                isNearCoords = true
+            end
+        end
+
+        Citizen.Wait(350)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+
+        if isNearCoords then
+            showInfobar("Test!")
+        end
+        
+        Citizen.Wait(1)
+    end
+end)
+
+
+function showInfobar(msg)
+    CurrentActionMsg  = msg
+    SetTextComponentFormat('STRING')
+    AddTextComponentString(CurrentActionMsg)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'delfin8557'
+description 'Geldwäsche by Delfin8557'
+version 'alpha-0.1'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
