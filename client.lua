6fabca2738fd0dde45c04025a355486026ea5282
Delfin8559 <164158308+Delfin8559@users.noreply.github.com>
2024-03-30 13:43:00 +0100

    blip changes

--- client.lua
+++ client.lua
@@ -1,27 +1,39 @@
 blip = nil
+blipRadius = nil
 
 RegisterCommand("sperrzone", function(source,agrs)
     if DoesBlipExist(blip) then
-        -- Blip Existiert Nachricht
+        ShowNotification("Eine Sperrzoe existiert schon!")
     else 
         local x, y, z = table.unpack(GetEntityCoords(PlayerPedId()))
-        TriggerServerEvent(blips:takeBlip)
+        TriggerServerEvent("blips:takeBlip", x, y, z)
     end
 end)
 
 RegisterNetEvent("blips:setBlip", function(x,y,z)
     if x ~= nil then
-        blip = AddBlipForCoords(blipCoords.x, blipCoords.y, blipCoords.z)
+        blip = AddBlipForCoord(x, y, z)
+        blipRadius = AddBlipForRadius(x,y,z, 1.0)
         SetBlipAsShortRange(blip, true)
 
         SetBlipScale(blip, 1.0)
         SetBlipSprite(blip, 161)
-        SetBlipColour(blip, 21)
-        SetBlipAlpha(blip, 255)
-        AddTextEntry("SPERRZONE", blipName)
+        SetBlipColour(blip, 3)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString('Sperrzone')
         EndTextCommandSetBlipName(blip)
         SetBlipCategory(blip, 2)
+
+        SetBlipAlpha(blipRadius, 80)
+
+        ShowNotification("Sperrzone wurde erstellt!")
     else
-       -- fehler 0x001, contact admin
+    	ShowNotification("Fehler 0x001, Melde dich im Support!")
     end
 end)
+
+function ShowNotification(text)
+	SetNotificationTextEntry('STRING')
+ 	AddTextComponentString(text)
+	DrawNotification(false, true)
+end
